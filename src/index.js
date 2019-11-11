1692dcd939848e873ce92764077e4759d937304e
Evan Vinciguerra <evan.vinciguerra@visionistinc.com>
2019-11-11 10:35:32 -0500

    second commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./style.css";
 
 const Index = () => {
-  return <div className="helloworld">Hello World!</div>;
+  return <div className="helloworld">Hello GIGANTIC World!</div>;
 };
 
 ReactDOM.render(<Index />, document.querySelector("#root"));
